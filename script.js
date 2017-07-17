cddb4fea0a3b98149518c3f27a213fb79e1fa4f5
Настя Безрук < >
2017-07-17 23:44:18 +0300

    fisrt try

--- script.js
+++ script.js
@@ -8,30 +8,40 @@ function initApp () {
 
 	if (startGame) {
 		randomNum = parseInt(getRandomArbitrary(0,5));
-		numResult = parseInt(prompt('Write your number'));
+		numResult = (prompt('Write your number?') !== null) ? parseInt(prompt('Write your number?')) : null;
 		debugger;
 
-		while (numResult !== randomNum && i < 3) {
+		while (numResult !== randomNum && numResult !== null && i < 3) {
 			numResult = parseInt(prompt('Write your number'));
 			i++;
 		}
 
-		if ( numResult === randomNum ) {
+		alertMsgs(numResult, randomNum);
+		
+
+	} else {
+		console.log("You didn't win million, but you could");
+	}
+}
+
+function alertMsgs(numResult, randomNum) {
+	var playAgain;
+
+	if ( numResult === randomNum ) {
 			if ( i === 1 ) {
 				alert('You won 10$');
 			} else if ( i === 2 ) {
 				alert('You won 5$');
 			} else {
 				alert('You won 2$');
-			}
+			}		
 		} else {
-			alert("You didn't win!");
+			alert("You won 0$");
+			playAgain = confirm('Do you want to play again?');
+			if (playAgain) {
+				initApp();
+			}
 		}
-		
-
-	} else {
-		console.log("You didn't win!");
-	}
 }
 
 function getRandomArbitrary(min, max) {
